0c0a1af5981530fbc96524447d1a777347e6c7f4
varunaditya27 <varunaditya2706@gmail.com>
2025-05-24 09:31:02 +0530

    created ToodoList component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,15 @@
-function TodoList () {
+import TodoItem from "./TodoItem.jsx"
+
+function TodoList (props) {
+    const { tasks, onDeleteTask } = props;
+
     return (
         <div>
-            
+            {
+                tasks.map(task => (
+                    <TodoItem key={task.id} task={task} onDeleteTask={onDeleteTask} />
+                ))
+            }
         </div>
     )
 }
