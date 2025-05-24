19e43fd36871a11cde576a9108b1d14704d3b1aa
varunaditya27 <varunaditya2706@gmail.com>
2025-05-24 09:30:25 +0530

    created TodoItem component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
 import TodoInput from "./TodoInput.jsx"
 
-function TodoItem () {
+function TodoItem (props) {
+    const { task, onDeleteTask } = props;
+
+    const handleDeleteTask = () => {
+        onDeleteTask(task.id)
+    }
+
     return (
         <div>
-            
+            <span>
+                <h3>{task.id} == {task.text} <button onClick={handleDeleteTask}>Delete</button></h3>
+            </span>
         </div>
     )
 }
